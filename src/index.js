5a3dac4fa5a6eb13cff56c842c4b53a3ac961844
Anders Strömberg <coltlaboy@yahoo.se>
2021-07-21 17:11:15 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { firebase } from "@firebase/app";
+const {
+  REACT_APP_FB_API_KEY,
+  REACT_APP_FB_AUTH_DOMAIN,
+  REACT_APP_FB_PROJECT_ID,
+  REACT_APP_FB_STORAGE_BUCKET,
+  REACT_APP_FB_MSG_SENDER_ID,
+  REACT_APP_FB_APP_ID,
+  REACT_APP_FB_MEASUREMENT_ID,
+} = process.env;
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="https://www.gstatic.com/firebasejs/8.6.8/firebase-analytics.js"></script>
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: REACT_APP_FB_API_KEY,
+  authDomain: REACT_APP_FB_AUTH_DOMAIN,
+  projectId: REACT_APP_FB_PROJECT_ID,
+  storageBucket: REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FB_MSG_SENDER_ID,
+  appId: REACT_APP_FB_APP_ID,
+  measurementId: REACT_APP_FB_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("evernoteContainer")
 );
 
 // If you want to start measuring performance in your app, pass a function
